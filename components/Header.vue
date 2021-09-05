e898d484317596444f6f9f34dd8ba51d582403f3
ICTNirob <71205100+ICTNirob@users.noreply.github.com>
2021-09-05 15:53:42 +0600

    updated

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <v-avatar
+    tile
+    color="blue"
+  >
+    <v-icon dark size="40">
+      mdi-dev-to
+    </v-icon>
+  </v-avatar> -->
+  <v-row no-gutters>
+    <v-col
+      cols="8"
+      offset-md="2"
+    >
+      <v-card
+        class="pa-2"
+        outlined
+      >
+        .col-12 .col-sm-6 .col-md-8
+
+        .col-12 .col-sm-6 .col-md-8
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
