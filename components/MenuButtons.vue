17f65b3548a3c1765ee8b9a3ca56d7bd13d10fd6
ICTNirob <71205100+ICTNirob@users.noreply.github.com>
2021-09-06 16:17:44 +0600

    updated

--- components/MenuButtons.vue
+++ components/MenuButtons.vue
@@ -1,15 +1,30 @@
 <template>
-  <v-card>
+  <v-card flat>
     <v-tabs
       color="deep-purple accent-4"
       right
     >
-      <v-tab>Landscape</v-tab>
-      <v-tab>City</v-tab>
-      <v-tab>Abstract</v-tab>
+      <v-toolbar-title><h3>Posts</h3></v-toolbar-title>
+      <v-spacer />
+      <v-tab>Feed</v-tab>
+      <v-tab>Week</v-tab>
+      <v-tab>Year</v-tab>
+      <v-tab>Month</v-tab>
+      <v-tab>Infinity</v-tab>
+      <v-tab>Latest</v-tab>
 
       <v-tab-item>
-        <v-container fluid>
+        <v-container>
+          <Feed />
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <Week />
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
           <v-row>
             <v-col
               v-for="i in 6"
@@ -26,7 +41,7 @@
         </v-container>
       </v-tab-item>
       <v-tab-item>
-        <v-container fluid>
+        <v-container>
           <v-row>
             <v-col
               v-for="i in 6"
@@ -43,7 +58,24 @@
         </v-container>
       </v-tab-item>
       <v-tab-item>
-        <v-container fluid>
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="`https://picsum.photos/500/300?image`"
+                aspect-ratio="1"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
           <v-row>
             <v-col
               v-for="i in 6"
